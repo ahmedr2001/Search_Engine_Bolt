b330ce1143af1f812b21dcb54cee96fd6d2e8a0e
AhmedZahran02 <ahmed.zahran02@eng-st.cu.edu.eg>
2023-05-05 15:03:22 +0300

    initializing spring boot

---
+++ BackEnd/Spring Boot Application/src/main/java/com/bolt/BoltBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bolt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoltBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoltBackendApplication.class, args);
+	}
+
+}
