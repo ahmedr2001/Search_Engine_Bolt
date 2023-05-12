0bf1ab997795aba2fe620a02939b768876aa4320
AhmedZahran02 <ahmed.zahran02@eng-st.cu.edu.eg>
2023-05-12 19:53:50 +0300

    some minor crawler improvements

--- BackEnd/Bolt/src/main/java/Crawler/test.java
+++ BackEnd/Bolt/src/main/java/Crawler/test.java
@@ -1,9 +1,31 @@
 package Crawler;
 
-import Logging.*;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 public class test {
-    public static void main(String[] args) {
-        Logging.printColored("test", Color.GREEN);
+    public static void main(String[] args) throws IOException {
+        String link ="https://twitter.com/tutorialspoint";
+        System.setProperty(
+                "webdriver.chrome.driver",
+                "./chromedriver.exe");
+        ChromeOptions options =new ChromeOptions();
+//        options.addArguments("--headless");
+        HashMap<String, Object> images = new HashMap<String, Object>();
+        images.put("images", 2);
+        HashMap<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("profile.default_content_setting_values", images);
+        options.setExperimentalOption("prefs", prefs);
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.get("https://www.geeksforgeeks.org");
+        Document doc = Jsoup.parse(driver.getPageSource());
+        System.out.println(doc.text());
+        driver.close();
     }
 }
